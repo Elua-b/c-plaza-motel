fbd9df9b725b6e050a9188f01da5ef9a46ba9052
Elua-b <bugingoeloi@gmail.com>
2022-08-11 13:45:59 +0200

    adding cheeck boxes

--- pages/products/[id].jsx
+++ pages/products/[id].jsx
@@ -1,21 +1,20 @@
-
 import styles from "../../styles/Products.module.css";
-import Image from 'next/image'
+import Image from "next/image";
 import { useState } from "react";
 const Products = () => {
-  const [size,setSize] =useState(0);
-    const pizza={
-        id:1,
-        img:'/pizza3.jpg',
-        name:'CAMPANOA',
-        price:[19.9,23.4,27.9],
-        desc:"lorem ipsum is oelready now to update on th cplaza motel Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknow Missingshor Must includenow to update on th cplaza motel Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknow Missingshor Must include  "
-    };
+  const [size, setSize] = useState(0);
+  const pizza = {
+    id: 1,
+    img: "/pizza3.jpg",
+    name: "CAMPANOA",
+    price: [19.9, 23.4, 27.9],
+    desc: "lorem ipsum is oelready now to update on th cplaza motel Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknow Missingshor Must includenow to update on th cplaza motel Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknow Missingshor Must include  ",
+  };
   return (
     <div className={styles.container}>
       <div className={styles.left}>
         <div className={styles.imgContainer}>
-          <Image src={pizza.img} layout="fill" alt=""/>
+          <Image src={pizza.img} layout="fill" alt="" />
         </div>
       </div>
       <div className={styles.right}>
@@ -24,23 +23,62 @@ const Products = () => {
         <p className={styles.desc}>{pizza.desc}</p>
         <h3 className={styles.choose}>Choose the size</h3>
         <div className={styles.sizes}>
-        <div className={styles.size} onClick={()=>setSize(0)}>
-          <Image src="/size.png" alt="" layout="fill" /> 
-          <span className={styles.number}>Large</span>
+          <div className={styles.size} onClick={() => setSize(0)}>
+            <Image src="/size.png" alt="" layout="fill" />
+            <span className={styles.number}>Small</span>
+          </div>
+          <div className={styles.size} onClick={() => setSize(1)}>
+            <Image src="/size.png" alt="" layout="fill" />
+            <span className={styles.number}>Medium</span>
+          </div>
+          <div className={styles.size} onClick={() => setSize(2)}>
+            <Image src="/size.png" alt="" layout="fill" />
+            <span className={styles.number}>Large</span>
+          </div>
         </div>
-        <div className={styles.size} onClick={()=>setSize(1)}>
-          <Image src="/size.png" alt="" layout="fill" /> 
-          <span className={styles.number}>Medium</span>
+        <h3 className={styles.choose}>Choose additional ingredients</h3>
+        <div className={styles.ingredients}>
+          <div className={styles.option}>
+            <input
+              type="checkbox"
+              id="double"
+              name="double"
+              className={styles.checkbox}
+            />
+            <label htmlFor="double">Double Ingredients</label>
+          </div><div className={styles.option}>
+            <input
+              type="checkbox"
+              id="cheese"
+              name="cheese"
+              className={styles.checkbox}
+            />
+            <label htmlFor="cheese">Extra Cheese</label>
+          </div><div className={styles.option}>
+            <input
+              type="checkbox"
+              id="spicy"
+              name="spicy"
+              className={styles.checkbox}
+            />
+            <label htmlFor="spicy">Spicy Sauce</label>
+          </div>
+          <div className={styles.option}>
+            <input
+              type="checkbox"
+              id="garlic"
+              name="garlic"
+              className={styles.checkbox}
+            />
+            <label htmlFor="garlic">Garlic Sauce</label>
+          </div>
         </div>
-        <div className={styles.size} onClick={()=>setSize(2)}>
-          <Image src="/size.png" alt="" layout="fill" /> 
-          <span className={styles.number}>Small</span>
+        <div className={styles.add}>
+          
         </div>
       </div>
-      </div>
-     
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
