af1bcabff269f951661477986af89598255f5b46
Elua-b <bugingoeloi@gmail.com>
2022-08-12 00:16:26 +0200

    ending  of frontend and functionarities

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -2,10 +2,18 @@ import styles from "../../styles/Orders.module.css";
 import Image from "next/image";
 
  const Orders = () => {
+    const status=0;
+    const statusClass=(index)=>{
+        if(index-status<1) return styles.done
+        if(index-status===1) return styles.inprogress
+        if(index-status>1) return styles.undone
+
+    }
   return (
     <div className={styles.container}>
         <div className={styles.left}>
             <div className={styles.row}>
+             
             <table className={styles.table}>
           <thead>
             <tr className={styles.tr}>
@@ -36,209 +44,44 @@ import Image from "next/image";
               </td>
             </tr>
           </tbody>
-          <tbody>
-            <tr>
-              
-              <td>
-                <span className={styles.id}>43255226436</span>
-              </td>
-              <td>
-                <span className={styles.name}>
-                 BUGINGO Eloi
-                </span>
-              </td>
-              <td>
-                <span className={styles.address}>NYAMIRAMBO KN.232-43</span>
-              </td>
-              
-              <td>
-                <span className={styles.total}>$39.80</span>
-              </td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              
-              <td>
-                <span className={styles.id}>43255226436</span>
-              </td>
-              <td>
-                <span className={styles.name}>
-                 BUGINGO Eloi
-                </span>
-              </td>
-              <td>
-                <span className={styles.address}>NYAMIRAMBO KN.232-43</span>
-              </td>
-              
-              <td>
-                <span className={styles.total}>$39.80</span>
-              </td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              
-              <td>
-                <span className={styles.id}>43255226436</span>
-              </td>
-              <td>
-                <span className={styles.name}>
-                 BUGINGO Eloi
-                </span>
-              </td>
-              <td>
-                <span className={styles.address}>NYAMIRAMBO KN.232-43</span>
-              </td>
-              
-              <td>
-                <span className={styles.total}>$39.80</span>
-              </td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              
-              <td>
-                <span className={styles.id}>43255226436</span>
-              </td>
-              <td>
-                <span className={styles.name}>
-                 BUGINGO Eloi
-                </span>
-              </td>
-              <td>
-                <span className={styles.address}>NYAMIRAMBO KN.232-43</span>
-              </td>
-              
-              <td>
-                <span className={styles.total}>$39.80</span>
-              </td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              
-              <td>
-                <span className={styles.id}>43255226436</span>
-              </td>
-              <td>
-                <span className={styles.name}>
-                 BUGINGO Eloi
-                </span>
-              </td>
-              <td>
-                <span className={styles.address}>NYAMIRAMBO KN.232-43</span>
-              </td>
-              
-              <td>
-                <span className={styles.total}>$39.80</span>
-              </td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              
-              <td>
-                <span className={styles.id}>43255226436</span>
-              </td>
-              <td>
-                <span className={styles.name}>
-                 BUGINGO Eloi
-                </span>
-              </td>
-              <td>
-                <span className={styles.address}>NYAMIRAMBO KN.232-43</span>
-              </td>
-              
-              <td>
-                <span className={styles.total}>$39.80</span>
-              </td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              
-              <td>
-                <span className={styles.id}>43255226436</span>
-              </td>
-              <td>
-                <span className={styles.name}>
-                 BUGINGO Eloi
-                </span>
-              </td>
-              <td>
-                <span className={styles.address}>NYAMIRAMBO KN.232-43</span>
-              </td>
-              
-              <td>
-                <span className={styles.total}>$39.80</span>
-              </td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              
-              <td>
-                <span className={styles.id}>43255226436</span>
-              </td>
-              <td>
-                <span className={styles.name}>
-                 BUGINGO Eloi
-                </span>
-              </td>
-              <td>
-                <span className={styles.address}>NYAMIRAMBO KN.232-43</span>
-              </td>
-              
-              <td>
-                <span className={styles.total}>$39.80</span>
-              </td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              
-              <td>
-                <span className={styles.id}>43255226436</span>
-              </td>
-              <td>
-                <span className={styles.name}>
-                 BUGINGO Eloi
-                </span>
-              </td>
-              <td>
-                <span className={styles.address}>NYAMIRAMBO KN.232-43</span>
-              </td>
-              
-              <td>
-                <span className={styles.total}>$39.80</span>
-              </td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              
-              <td>
-                <span className={styles.id}>43255226436</span>
-              </td>
-              <td>
-                <span className={styles.name}>
-                 BUGINGO Eloi
-                </span>
-              </td>
-              <td>
-                <span className={styles.address}>NYAMIRAMBO KN.232-43</span>
-              </td>
-              
-              <td>
-                <span className={styles.total}>$39.80</span>
-              </td>
-            </tr>
-          </tbody>
+          
           
         </table>
             </div>
+            <div className={styles.row}>
+            <div className={statusClass(0)}>
+                    <Image src="/paid.png" alt=""  width={30} height={30}/>
+                    <span>Payment</span>
+                    <div className={styles.checkedIcon}>
+                
+                        <Image className="checkedIcon" src="/checked.png" alt=""  width={20} height={20}/>
+                    </div>
+                </div>
+                <div className={statusClass(1)}>
+                    <Image src="/bake.png" alt=""  width={30} height={30}/>
+                    <span>Preparing</span>
+                    <div className={styles.checkedIcon}>
+                
+                        <Image className="checkedIcon" src="/checked.png" alt=""  width={20} height={20}/>
+                    </div>
+                </div>
+                <div className={statusClass(2)}>
+                    <Image src="/bike.png" alt=""  width={30} height={30}/>
+                    <span>On the way</span>
+                    <div className={styles.checkedIcon}>
+                
+                        <Image className="checkedIcon" src="/checked.png" alt=""  width={20} height={20}/>
+                    </div>
+                </div>
+                <div className={statusClass(3)}>
+                    <Image  src="/delivered.png" alt=""  width={30} height={30}/>
+                    <span>Delivered</span>
+                    <div className={styles.checkedIcon}>
+                
+                        <Image  className="checkedIcon" src="/checked.png" alt=""  width={20} height={20}/>
+                    </div>
+                </div>
+                </div>
         </div>
         <div className={styles.right}>
         <div className={styles.wrapper}>
