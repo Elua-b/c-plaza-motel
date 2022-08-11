e638b680457e3e568e22ced4076e73fb2d94d311
Elua-b <bugingoeloi@gmail.com>
2022-08-11 12:53:57 +0200

    state management to controll sizes

--- pages/products/[id].jsx
+++ pages/products/[id].jsx
@@ -24,15 +24,15 @@ const Products = () => {
         <p className={styles.desc}>{pizza.desc}</p>
         <h3 className={styles.choose}>Choose the size</h3>
         <div className={styles.sizes}>
-        <div className={styles.size}>
+        <div className={styles.size} onClick={()=>setSize(0)}>
           <Image src="/size.png" alt="" layout="fill" /> 
           <span className={styles.number}>Large</span>
         </div>
-        <div className={styles.size}>
+        <div className={styles.size} onClick={()=>setSize(1)}>
           <Image src="/size.png" alt="" layout="fill" /> 
           <span className={styles.number}>Medium</span>
         </div>
-        <div className={styles.size}>
+        <div className={styles.size} onClick={()=>setSize(2)}>
           <Image src="/size.png" alt="" layout="fill" /> 
           <span className={styles.number}>Small</span>
         </div>
