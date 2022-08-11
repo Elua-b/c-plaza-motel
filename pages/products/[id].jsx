be39e8a641b578d9b9c94365166737ffae1a3bb1
Elua-b <bugingoeloi@gmail.com>
2022-08-11 12:24:13 +0200

    beautifying product page

--- pages/products/[id].jsx
+++ pages/products/[id].jsx
@@ -1,13 +1,15 @@
 
 import styles from "../../styles/Products.module.css";
 import Image from 'next/image'
+import { useState } from "react";
 const Products = () => {
+  const [size,setSize] =useState(0);
     const pizza={
         id:1,
         img:'/pizza3.jpg',
         name:'CAMPANOA',
         price:[19.9,23.4,27.9],
-        desc:"lorem ipsum is oelready now to update on th cplaza motel"
+        desc:"lorem ipsum is oelready now to update on th cplaza motel Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknow Missingshor Must includenow to update on th cplaza motel Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknow Missingshor Must include  "
     };
   return (
     <div className={styles.container}>
@@ -16,7 +18,13 @@ const Products = () => {
           <Image src={pizza.img} layout="fill" alt=""/>
         </div>
       </div>
-      <div className={styles.right}></div>
+      <div className={styles.right}>
+        <h1 className={styles.title}>{pizza.name}</h1>
+        <span className={styles.price}>${pizza.price[size]}</span>
+        <p className={styles.desc}>{pizza.desc}</p>
+        <h3 className={styles.choose}>Choose the size</h3>
+      </div>
+      <div></div>
     </div>
   )
 }
