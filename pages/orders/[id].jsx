72f0e0381ce1bc4496783d4eeebc8f7eb9797948
Elua-b <bugingoeloi@gmail.com>
2022-08-11 23:33:50 +0200

    adding the right side of orders page

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -4,7 +4,42 @@ import Image from "next/image";
  const Orders = () => {
   return (
     <div className={styles.container}>
-        <div className={styles.left}></div>
+        <div className={styles.left}>
+            <div className={styles.row}>
+            <table className={styles.table}>
+          <thead>
+            <tr className={styles.tr}>
+              <th>Order ID</th>
+              <th>Customer</th>
+              <th>Address</th>
+              <th>Total</th>
+              
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              
+              <td>
+                <span className={styles.id}>43255226436</span>
+              </td>
+              <td>
+                <span className={styles.name}>
+                 BUGINGO Eloi
+                </span>
+              </td>
+              <td>
+                <span className={styles.address}>NYAMIRAMBO KN.232-43</span>
+              </td>
+              
+              <td>
+                <span className={styles.total}>$39.80</span>
+              </td>
+            </tr>
+          </tbody>
+          
+        </table>
+            </div>
+        </div>
         <div className={styles.right}>
         <div className={styles.wrapper}>
           <h2 className={styles.title}>CART TOTAL</h2>
