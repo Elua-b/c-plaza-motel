eeb47c92043620fe81932d445aa84d80287a3def
Elua-b <bugingoeloi@gmail.com>
2022-08-11 23:17:20 +0200

    completing the cart page

---
+++ pages/orders/[id].jsx
@@ -0,0 +1,12 @@
+import styles from "../../styles/Orders.module.css";
+import Image from "next/image";
+
+ const Orders = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.left}></div>
+        <div className={styles.right}></div>
+    </div>
+  )
+}
+export default  Orders
