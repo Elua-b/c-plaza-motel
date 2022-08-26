30c82d74ad13216defe9e16b1817d5c3a14e0d98
Elua-b <bugingoeloi@gmail.com>
2022-08-26 16:59:58 +0200

    updating prices

--- pages/products/[id].jsx
+++ pages/products/[id].jsx
@@ -1,10 +1,28 @@
 import styles from "../../styles/Products.module.css";
-import axios from 'axios'
+import axios from "axios";
 import Image from "next/image";
 import { useState } from "react";
-const Products = ({product}) => {
+const Products = ({ product }) => {
+  const [price, setPrice] = useState(product.prices[0]);
   const [size, setSize] = useState(0);
- 
+  const changePrice = (number) => {
+    setPrice(price + number);
+  };
+
+  const handleSize = (sizeIndex) => {
+    const difference = product.prices[sizeIndex] - product.prices[size];
+    setSize(sizeIndex);
+    changePrice(difference)
+  };
+  const handleChange = (e, option) => {
+    const checked = e.target.checked;
+    if(checked){
+      changePrice(option.price)
+    }
+    else{
+      changePrice(-option.price)
+    }
+  };
   return (
     <div className={styles.container}>
       <div className={styles.left}>
@@ -14,54 +32,56 @@ const Products = ({product}) => {
       </div>
       <div className={styles.right}>
         <h1 className={styles.title}>{product.name}</h1>
-        <span className={styles.price}>${product.prices[size]}</span>
+        <span className={styles.price}>${price}</span>
         <p className={styles.desc}>{product.desc}</p>
         <h3 className={styles.choose}>Choose the size</h3>
         <div className={styles.sizes}>
-          <div className={styles.size} onClick={() => setSize(0)}>
+          <div className={styles.size} onClick={() => handleSize(0)}>
             <Image src="/size.png" alt="" layout="fill" />
             <span className={styles.number}>Small</span>
           </div>
-          <div className={styles.size} onClick={() => setSize(1)}>
+          <div className={styles.size} onClick={() => handleSize(1)}>
             <Image src="/size.png" alt="" layout="fill" />
             <span className={styles.number}>Medium</span>
           </div>
-          <div className={styles.size} onClick={() => setSize(2)}>
+          <div className={styles.size} onClick={() => handleSize(2)}>
             <Image src="/size.png" alt="" layout="fill" />
             <span className={styles.number}>Large</span>
           </div>
         </div>
         <h3 className={styles.choose}>Choose additional ingredients</h3>
         <div className={styles.ingredients}>
-          {product.extraOptions.map((option)=>{
-return(
-  <div className={styles.option} key={option._id}>
-            <input
-              type="checkbox"
-              id="double"
-              name="double"
-              className={styles.checkbox}
-            />
-            <label htmlFor="double">{option.text}</label>
-          </div>
-)
+          {product.extraOptions.map((option) => {
+            return (
+              <div className={styles.option} key={option._id}>
+                <input
+                  type="checkbox"
+                  id={option._id}
+                  name={option.text}
+                  className={styles.checkbox}
+                  onChange={(e) => handleChange(e, option)}
+                />
+                <label htmlFor="double">{option.text}</label>
+              </div>
+            );
           })}
-          
         </div>
         <div className={styles.add}>
-          <input type="number" defaultValue={1} className={styles.quantity}  />
+          <input type="number" defaultValue={1} className={styles.quantity} />
           <button className={styles.button}>Add to Cart</button>
         </div>
       </div>
     </div>
   );
 };
-export const getServerSideProps=async({params})=>{
-  const res=await axios.get(`http://localhost:3000/api/products/${params.id}`)
-  return{
-    props:{
-      product:res.data
-    }
-  }
-  }
+export const getServerSideProps = async ({ params }) => {
+  const res = await axios.get(
+    `http://localhost:3000/api/products/${params.id}`
+  );
+  return {
+    props: {
+      product: res.data,
+    },
+  };
+};
 export default Products;
