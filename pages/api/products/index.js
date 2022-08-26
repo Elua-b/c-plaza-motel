8f53e6766c0e1aa50055c8410a9f2e9b86b32bb6
Elua-b <bugingoeloi@gmail.com>
2022-08-26 11:38:55 +0200

    adding create controller

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,4 +1,26 @@
-export default function handler(req, res) {
-    res.status(200).json({ name: 'John Doe' })
+import { dbconnection } from "../../../util/mongo"
+import Product from "../../../models/Product"
+export default async function handler(req, res) {
+   const {method}=req
+   dbconnection()
+   if(method==="GET"){
+    try {
+        const product=await Product.find();   
+        res.status(200).json(product)     
+    } catch (error) {
+        res.status(500).json(error)
+    }
+    
+   }
+   if(method==="POST"){
+    try {
+        // req.body
+        const product = await Product.create(req.body)
+        res.status(201).json({product})
+    } catch (error) {
+        res.status(500).json(error)
+    }
+   }
+
   }
   
