e2befbe8af8689d8f945f669d85401531424198e
Elua-b <bugingoeloi@gmail.com>
2022-08-28 10:21:37 +0200

    orders

--- pages/api/orders/index.jsx
+++ pages/api/orders/index.jsx
@@ -3,7 +3,22 @@ import Order from "../../../models/Order";
 
 const handler=async(req,res)=>{
     const {method}=req;
-    if(method==="GET"){}
-    if(method==="POST"){}
+    await dbconnection();
+    if(method==="GET"){
+        try {
+            const orders=await Order.find()
+            res.status(200).json(orders);
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+    if(method==="POST"){
+        try {
+            const orders=await Order.find(req.body)
+            res.status(201).json(orders);
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
 }
 export default handler
